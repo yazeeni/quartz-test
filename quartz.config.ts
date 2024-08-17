52233e52eb77ed606acdea86c00c86e63729807d
yazeeni <yasin.nam@gmx.de>
2024-08-17 23:31:31 +0200

    changed locale to de-DE

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
+    locale: "de-DE",
     baseUrl: "yazeeni.github.io",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
